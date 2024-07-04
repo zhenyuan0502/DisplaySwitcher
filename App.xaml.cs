06d415df67e76243677c6ee938f568f4e356f221
Luu Tan Nguyen <giangchannguyen@gmail.com>
2024-07-05 03:04:50 +0700

    Enhanced app with JumpList and display switching
    - Added a new resource string `AppTitleName` in App.xaml for the application title.
    - Introduced JumpList functionality in App.xaml.cs, including the addition of `using Windows.UI.StartScreen;`, a new `LaunchParam` property, and asynchronous handling in `OnLaunched` method. Also added logic for managing JumpList items.
    - Modified DisplaySwitcher.csproj by removing the application manifest setting, enabling `AllowUnsafeBlocks`, setting a `PackageIcon`, and changing how `TitlebarLogo.png` is handled. Included new resources management files for better resource handling.
    - Updated MainWindow.xaml to have a vertical `StackPanel` and replaced a single button with multiple buttons for different display modes, each with specific event handlers.
    - Extended MainWindow.xaml.cs with additional `using` directives, private fields for display paths, event handlers for new UI buttons, and methods for executing display mode switching.
    - Updated the application display name in Package.appxmanifest to `Display Switcher`.
    - Added `Resources.Designer.cs` and `Resources.resx` for managing localized resources in a strongly-typed manner.

--- App.xaml.cs
+++ App.xaml.cs
@@ -15,6 +15,7 @@ using Windows.ApplicationModel;
 using Windows.ApplicationModel.Activation;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.StartScreen;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -35,14 +36,44 @@ namespace DisplaySwitcher
             this.InitializeComponent();
         }
 
+        public string LaunchParam { get; set; }
+
         /// <summary>
         /// Invoked when the application is launched.
         /// </summary>
         /// <param name="args">Details about the launch request and process.</param>
-        protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
+        protected override async void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
         {
+            LaunchParam = args.Arguments;
+            // Show message log in the console
+            Console.WriteLine($"LaunchParam: {LaunchParam}");
+
             m_window = new MainWindow();
             m_window.Activate();
+            m_window.AppWindow.Resize(new (480, 800));
+            m_window.AppWindow.SetIcon("Assets/TitlebarLogo.png");
+
+            var jumpList = await JumpList.LoadCurrentAsync();
+            jumpList.Items.Clear();
+            await jumpList.SaveAsync();
+
+            var list = new List<string>() { "First", "Second", "Duplicate", "Extend" };
+            bool isChanged = false;
+            foreach (var item in list)
+            {
+                if (jumpList.Items.Any(x => x.Arguments == item))
+                    continue;
+
+                var jumpListItem = JumpListItem.CreateWithArguments(item, item);
+                jumpListItem.Description = "ms-resource:///Resources/CustomJumpListItemDescription";
+                jumpListItem.GroupName = "";
+                jumpListItem.Logo = new Uri("ms-appx:///Assets/TitlebarLogo.png");
+                jumpList.Items.Add(jumpListItem);
+                isChanged = true;
+            }
+
+            if (isChanged)
+                await jumpList.SaveAsync();
         }
 
         private Window m_window;
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -7,11 +7,19 @@ using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Devices.Enumeration;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Popups;
+using Windows.UI.Shell;
+using Windows.UI.StartScreen;
+
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -23,14 +31,68 @@ namespace DisplaySwitcher
     /// </summary>
     public sealed partial class MainWindow : Window
     {
+        private string m_windowsPath = Environment.GetFolderPath(Environment.SpecialFolder.Windows);
+        private string m_displaySwitchPath = string.Empty;
         public MainWindow()
         {
             this.InitializeComponent();
+            m_displaySwitchPath = Path.Combine(m_windowsPath, "System32", "DisplaySwitch");
         }
 
-        private void myButton_Click(object sender, RoutedEventArgs e)
+        private async void PinTaskBar_Click(object sender, RoutedEventArgs e)
         {
-            myButton.Content = "Clicked";
+            //pinTaskBar.IsEnabled = false;
+            //var isPinned = await TaskbarManager.GetDefault().RequestPinCurrentAppAsync();
+
+            //// Update the UI to the appropriate state based on the results of the pin request.
+            //pinTaskBar.IsEnabled = !isPinned;
+            //pinTaskBar.IsEnabled = true;
+
+            // Get the app's jump list.
+            var jumpList = await Windows.UI.StartScreen.JumpList.LoadCurrentAsync();
+
+            // Set the system to autogenerate a Frequent group for the app jump list.
+            // Alternatively, this property could be set to JumpListSystemGroupKind.Recent to autogenerate a Recent group.
+            jumpList.SystemGroupKind = Windows.UI.StartScreen.JumpListSystemGroupKind.Frequent;
+
+            // No changes were made to the jump list Items property, so any custom tasks and groups remain intact.
+            await jumpList.SaveAsync();
         }
+
+        private void StartScreen(int number)
+        {
+            Process process = new Process();
+            process.StartInfo.FileName = "cmd.exe";
+            process.StartInfo.Arguments = $"/c {m_displaySwitchPath} {number}";
+            process.StartInfo.CreateNoWindow = true;
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.RedirectStandardInput = true;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.RedirectStandardError = true;
+            process.Start();
+            process.WaitForExit();
+            process.Close();
+        }
+
+        private void FirstScreen_Click(object sender, RoutedEventArgs e)
+        {
+            StartScreen(1);
+        }
+
+        private void SecondScreen_Click(object sender, RoutedEventArgs e)
+        {
+            StartScreen(4);
+        }
+
+        private void DuplicateScreen_Click(object sender, RoutedEventArgs e)
+        {
+            StartScreen(2);
+        }
+
+        private void ExtendScreen_Click(object sender, RoutedEventArgs e)
+        {
+            StartScreen(3);
+        }
+
     }
 }
